ff0e9e641e1a258ffe49e1fbccf59162f1a2ee09
Alex <acleclair@gmail.com>
2019-09-25 15:08:19 -0400

    update

--- examples.py
+++ examples.py
@@ -11,7 +11,7 @@ train_csv = '/deepData/DataSetXfer/DataSets/tcia-ddsm-all/ClassicalPathFormat/ma
 test_csv = '/deepData/DataSetXfer/DataSets/tcia-ddsm-all/ClassicalPathFormat/mass_case_description_test_set.csv'
 
 # output location
-outpath = '~/data/train/'
+outpath = '../data/train/'
 
 train_csv_data = pandas.read_csv(train_csv)
 # Check out pandas docs on to_dict function - https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.to_dict.html
